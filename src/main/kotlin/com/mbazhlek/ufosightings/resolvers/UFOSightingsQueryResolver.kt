0a100d693bf061d6a5a50e2a4970619112c323de
Maria B <mbazhlek@gmail.com>
2020-01-26 12:45:58 -0800

    feat: add ufoSightingById and ufoSightingsByCountry queries

--- src/main/kotlin/com/mbazhlek/ufosightings/resolvers/UFOSightingsQueryResolver.kt
+++ src/main/kotlin/com/mbazhlek/ufosightings/resolvers/UFOSightingsQueryResolver.kt
@@ -5,10 +5,31 @@ import com.mbazhlek.ufosightings.entity.UFOSighting
 import com.mbazhlek.ufosightings.repository.UFOSightingRepository
 import org.springframework.stereotype.Component
 
+
 @Component
 class UFOSightingsQueryResolver(val ufoSightingRepository: UFOSightingRepository) : GraphQLQueryResolver {
 
     fun ufoSightings(): List<UFOSighting> {
         return ufoSightingRepository.findAll()
+    }
+
+    fun ufoSightingById(id: String): UFOSighting? {
+        return ufoSightingRepository.findById(id)
+                .orElse(null)
+    }
+
+    fun ufoSightingsByCountry(country: String): List<UFOSighting> {
+        return findAllByCountry(country)
+    }
+
+    // TO DO: implement query
+    fun ufoSightingsByDayOfTheWeek(day: String): List<UFOSighting> {
+        return emptyList()
+    }
+
+    private fun findAllByCountry(country: String): List<UFOSighting> {
+        return ufoSightingRepository
+                .findAll()
+                .filter { it.country == country }
     }
 }
